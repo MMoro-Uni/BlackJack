01d4d4845ea1a0332d230125ef9bb366a177dbd7
MMoro <m.moro15@studenti.uniba.it>
2025-06-17 10:48:07 +0200

    doxygen fix

--- Server/dbfunctions.c
+++ Server/dbfunctions.c
@@ -2,44 +2,51 @@
 * \file dbfunctions.c
 * \brief Contains code for managing the database.
 *		 Code inside this file and its header is not meant for usage in other projects.
-* 
-* \fn int login_callback(void* data, int columns, char** row_fields, char** row_names)
+*/ 
+
+/*! \fn int login_callback(void* data, int columns, char** row_fields, char** row_names)
 * \brief callback function for SQLite SELECT
 * the function doesn't do much with the data it gets in input, but returns the amount of columns matching details given.
 * if the return value is >0 that means something was found with the given credentials, meaning login was successful.
-* 
-* \fn int GameLogin(char username[], char password[], sqlite3* ConnectedDB)
+*/
+
+/*! \fn int GameLogin(char username[], char password[], sqlite3* ConnectedDB)
 * \brief function to login into database
 * Tries to log into databse using given username and password.
 * Data is looked for in a LoginTable table containing usernames and their associated passwords.
 * LoginTable must be inside the sqlite3 object passed to the function.
-* 
-* \fn int GameRegister(char username[], char password[], sqlite3* ConnectedDB)
+*/
+
+/*! \fn int GameRegister(char username[], char password[], sqlite3* ConnectedDB)
 * \brief function to register into database
 * Creates a new row in the login table and creates a new row in the money table for given user.
 * Data is created in LoginTable and MoneyTable.
 * Tables must be inside the sqlite3 object passes to the function.
-* 
-* \fn int AlterMoney(char username[], char password[], int value_modifier, sqlite3* ConnectedDB)
+*/
+
+/*! \fn int AlterMoney(char username[], char password[], int value_modifier, sqlite3* ConnectedDB)
 * \brief alters the amount of money the given user has by value_modifier
 * The function first checks if the user exists in LoginTable through GameLogin()
 * If user exists, the username is used to modify the correct value in MoneyTable.
 * value_modifier is added to the Value field in the selected MoneyTable row.
 * LoginTable and MoneyTable must be inside the given sqlite3 object.
-* 
-* \fn int GetMoney(char username[], char password[], char money_buffer[],  sqlite3* ConnectedDB)
+*/
+
+/*! \fn int GetMoney(char username[], char password[], char money_buffer[],  sqlite3* ConnectedDB)
 * \brief gets the amount of money the given user has and puts it into money_buffer
 * The function first checks if the user exists in LoginTable through GameLogin()
 * If user exists, the username is used to get the correct value from MoneyTable.
 * The obtained value is then copied as a string into money_buffer[].
 * Tables must be inside the sqlite3 object passed to the function.
-* 
-* \int get_money_callback(void* var_pointer, int columns, char** row_fields, char** row_names)
+*/
+
+/*! \fn int get_money_callback(void* var_pointer, int columns, char** row_fields, char** row_names)
 * \brief callback function for GetMoney()
 * After the value to put into money_buffer has been obtained by the callback function, it is put into var_pointer
 * For correct functioning, var_pointer must be a pointer to money_buffer.
-* 
-* 
+*/
+
+/* 
 * Possible return values for functions:
 * DB_CONN_FAIL: could not connect to database
 * DB_ERROR: error in writing data to the database
@@ -48,7 +55,6 @@
 * REG_DONE: registration process was completed successfully
 * UPDATE_DONE: the amount of money the player has was updated successfully
 * OBTAIN_DONE: the amount of money the player has was obtained successfully
-* 
 */
 
 
