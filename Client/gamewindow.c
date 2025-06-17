cf126848556493335699f5e4053ba1bcd1220478
MMoro <m.moro15@studenti.uniba.it>
2025-06-17 10:40:14 +0200

    commenti aggiornati + commenti doxygen

--- Client/gamewindow.c
+++ Client/gamewindow.c
@@ -1,3 +1,64 @@
+/*!
+* \file gamewindow.c
+* \brief contains code for game window drawing and game logic.
+* The code in this file is not meant for usage in other projects. Don't include without modifying first.
+*/
+
+/*!
+* \fn void GameWindow_init(float gw_screen_width, float gw_screen_height)
+* \brief init function for the game window
+* Global variables are used to avoid having to pass window sizes to every function and to avoid updates of values when no changes were made to the window.
+* This also avoids the deallocation and reallocation of the same size of memory for variables when the function gets called again for retry.
+* Variables used for the game logic are initialized every time the function is called.
+* Variables of which the values are tied to the screen size are only initialized if the screen size has changed from the last call.
+* This is done through the global variables screen_width and screen_height, which store the size of the previous window and are compared to the passed values.
+* 
+* NOTE: Buttons are stored in an array.
+* Current values: ButtonArray[0] = hit, ButtonArray[1] = stand, ButtonArray[2] = surrend, ButtonArray[3] = double down, ButtonArray[4] = bet
+* this is done to facilitate adding new buttons if needed for modifications to the game.
+*/
+
+/*!
+* \fn int GameStart(ONMODE online_mode);
+* \brief main function for game logic and window drawing.
+* online_mode is used to determine if the function should call client functions for server calls to handle the money amount the player has.
+* return value is 1 if the game is supposed to restart, 0 if the game should end.
+* Currently, the only way of closing the game is through closing the window, so 0 should not be returned, but the value is still returned in case of future added features.
+* NOTE: this function is the only function in the file with a loop, so other functions are called during the loop for drawing the screen.
+*/
+
+/*!
+* \fn void window_draw_card(CARD card_to_draw, Rectangle place_to_draw)
+* \brief utility function for drawing a given card in the given position.
+*/
+
+/*!
+* \fn void draw_end_screen_init()
+* \brief init function for draw_end_screen
+* see GameWindow_init() for reason of the usage of init functions
+*/
+
+/*!
+* \fn void draw_end_screen(SIGNAL mode)
+* \brief draws the end screen
+*        mode is used to determine text to draw on the end screen
+* This functions also changes the value of the global bool retry if the retry button is pressed.
+*/
+
+/*!
+* \fn void draw_bet_screen_init()
+* \brief init function for draw_bet_screen
+* see GameWindow_init() for reason of usage of init functions
+*/
+
+/*
+* \fn int draw_bet_screen()
+* \brief draws the screen for placing bets.
+*        return value is used to return the bet
+*/
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -15,6 +76,7 @@
 #define START_CARD_NUMBER 2
 #define BLACKJACK_SCORE 21
 #define MIN_DEALER_STAND_SCORE 17
+//suit symbols are done through a font. These are the characters that have those symbols in the font.
 #define CLUBS "}"
 #define DIAMONDS "{"
 #define HEARTS "<"
@@ -28,43 +90,50 @@ void draw_end_screen_init();
 void draw_end_screen(SIGNAL mode);
 void draw_bet_screen_init();
 int draw_bet_screen();
-//global variable initialization
 
-//GameWindow related global variables
+//GameWindow and file wide global variables
+//variables for other functions that had to be global for usage of init functions are declare before the init function
 static CARD PlayerHand[MAX_CARD_NUMBER] = { 0 };
 static CARD DealerHand[MAX_CARD_NUMBER] = { 0 };
 static DECK GameDeck;
+static SIGNAL endgame_code;
+
 static int player_card_number;
 static int player_hand_score;
 static int dealer_card_number;
 static int dealer_hand_score;
 static int money_value;
 static int money_bet;
-static SIGNAL endgame_code;
+static int rel_x;
+static int rel_y;
+
 static char money_value_string[DEFAULT_BUFLEN];
 static char money_bet_string[DEFAULT_BUFLEN];
-static bool end_game;
-static bool retry;
-static bool set_bet;
-static bool updated_money;
+
 static float screen_width = 0;
 static float screen_height = 0;
+
 static Font CardFont;
 static Font SuitFont;
 static Font DefaultFont;
+
 static Rectangle PlayerCardRecArray[MAX_CARD_NUMBER];
 static Rectangle DealerCardRecArray[MAX_CARD_NUMBER];
 static Rectangle ButtonArray[BUTTON_NUMBER];
 static Rectangle ButtonAreaBGRec;
+
 static Vector2 MoneyTextVec;
 static Vector2 MoneyBetTextVec;
+
 static bool hit_button_pressed;
 static bool stand_button_pressed;
 static bool surrend_button_pressed;
 static bool doubledown_button_pressed;
 static bool bet_button_pressed;
-static int rel_x;
-static int rel_y;
+static bool end_game;
+static bool retry;
+static bool set_bet;
+static bool updated_money;
 static bool button_collision;
 static bool sizes_changed;
 static bool end_screen_changed;
@@ -168,6 +237,7 @@ void GameWindow_init(float gw_screen_width, float gw_screen_height)
 
     if (sizes_changed)
     {
+        //explanation of formula used at the end of the file
         float width = PlayerCardRecArray[0].width;
         float height = PlayerCardRecArray[0].height;
         float card_ratio = width / height;
@@ -204,6 +274,7 @@ int GameStart(ONMODE online_mode)
 
     GameWindow_init(gw_screen_width, gw_screen_height);
 
+    //only needs to be called once since after retry the function gets called again
     if (online_mode == ONLINE_PLAY)
     {
         money_value = money_value_get();
@@ -212,6 +283,7 @@ int GameStart(ONMODE online_mode)
     while (!WindowShouldClose())
     {
 
+        //checking button pressed and end game condition
         if (!end_game && !set_bet)
         {
             if (hit_button_pressed && player_card_number < MAX_CARD_NUMBER)
@@ -499,11 +571,13 @@ void window_draw_card(CARD card_to_draw, Rectangle place_to_draw)
 }
 
 static Rectangle EndMessageBGRec;
-static Vector2 EndscreenTextPos;
-static char EndMessage[DEFAULT_BUFLEN];
-static Vector2 EndTextSize;
 static Rectangle RetryButtonRec;
+
+static Vector2 EndscreenTextPos;
+static Vector2 EndTextSize;
 static Vector2 EndTextFinalPos;
+
+static char EndMessage[DEFAULT_BUFLEN];
 
 
 void draw_end_screen_init()
@@ -585,6 +659,7 @@ void draw_end_screen(SIGNAL mode)
 static Rectangle BetWindowBGRec;
 static Rectangle BetInputBoxRec;
 static Rectangle BetButtonRec;
+
 static Vector2 BetLabelPositionVec;
 
 
@@ -670,3 +745,18 @@ int draw_bet_screen()
         return return_value;
     }
 }
+
+
+
+
+/*
+* EXPLANATION OF FORMULA USED IN GameWindow_init
+* 
+* the goal of the formula is to find a point on the diagonal of a rectangle given its height and its width
+* 
+* We can think of the segment of the diagonal that corresponds to a given size (in this case 10% of its size) as the diagonal of a smaller rectangle of unknow height and width.
+* From the height and width of the card rectangle we can ovtain the ratio of the two sizes, which will remain the same in the smaller rectangle we are considering.
+* By substituting in the formula for the diagonal the x with y * ratio we can then obtain that the diagonal is equal to the square root of y^2 * (1 + ratio^2).
+* By taking the square of this formula (so diagonal^2 and remove the square root) we can then divide the square of the diagonal by (1 + ratio^2) to obtain y^2.
+* After finding the square root of the obtained y, we can multiply it by the ratio to find the value of x, finding the point on the diagonal of the caard square.
+*/
