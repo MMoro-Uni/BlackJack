780a8a95df00933a42d078e8e74e013581a55147
MMoro <m.moro15@studenti.uniba.it>
2025-06-17 10:02:34 +0200

    commenti doxygen aggiornati

--- Client/gamemenu.h
+++ Client/gamemenu.h
@@ -1,3 +1,9 @@
+/*!
+* \file gamemenu.h
+* \brief header that contains declaration of MainMenu() from the .c file.
+*        also contains enum online_mode for the current connection state.
+*/
+
 typedef enum online_mode
 {
     ONLINE_PLAY = 10,
